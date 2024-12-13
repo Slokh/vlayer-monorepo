16a39c7f42a7c2881469ad35d116de57fc6a1fef
Kartik <slokhx@gmail.com>
2024-12-13 16:30:00 -0500

    fix

--- ethereum/oracles/src/noir/oracles/server/types.ts
+++ ethereum/oracles/src/noir/oracles/server/types.ts
@@ -1,16 +1,10 @@
 // Taken from here: https://noir-lang.org/docs/how_to/how-to-oracles
 
-export interface SingleForeignCallParam {
-  Single: string;
-}
-
-export interface ArrayForeignCallParam {
-  Array: string[];
-}
-
+export type SingleForeignCallParam = string;
+export type ArrayForeignCallParam = string[];
 export type ForeignCallParam = SingleForeignCallParam | ArrayForeignCallParam;
 export type ForeignCallParams = ForeignCallParam[];
 
 export interface ForeignCallResult {
-  values: ForeignCallParams;
+	values: ForeignCallParams;
 }
